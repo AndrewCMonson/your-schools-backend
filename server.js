e1df4a05bcc3cfff275cd287e490460e2eab7f5a
Andrew Monson <andrewmonsondev@gmail.com>
2024-08-23 14:26:38 -0400

    updated cors urls

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 import express from "express";
-import dotenv from "dotenv";
-import process from "process";
 import cors from "cors";
-import path, { dirname } from "path";
 import connectDB from "./config/db";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { typeDefs, resolvers } from "./schemas/index";
 import { authMiddleware } from "./utils/auth";
-import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
-dotenv.config();
+import { AWSSecretsRetrieval } from "./env.config";
+const { CLOUDFRONT_URL } = await AWSSecretsRetrieval();
 const PORT = process.env.PORT || 3005;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const corsOptions = {
-    origin: [`${process.env.CLOUDFRONT_URL}`, "http://localhost:5173"],
+    origin: [
+        `https://${process.env.CLOUDFRONT_URL}`,
+        "https://localhost:5173",
+        `https://${CLOUDFRONT_URL}`,
+    ],
     credentials: true,
 };
 const startServer = async () => {
@@ -28,12 +27,6 @@ const startServer = async () => {
     app.use("/graphql", express.json(), cors(corsOptions), cookieParser(), express.urlencoded({ extended: true }), expressMiddleware(server, {
         context: authMiddleware,
     }));
-    if (process.env.NODE_ENV === "production") {
-        app.use(express.static(path.join(__dirname, "../frontend/dist")));
-        app.get("*", (_, res) => {
-            res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-        });
-    }
     connectDB();
     app.listen(PORT, () => {
         console.log(`Server listening on ${PORT}`);
