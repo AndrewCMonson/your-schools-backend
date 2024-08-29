483d6804b4bbebb2f5d41f3ef7782392750f827e
Andrew Monson <andrewmonsondev@gmail.com>
2024-08-29 09:45:40 -0400

    gitignore reset

--- services/AWSSecretsRetrieval.test.ts
+++
@@ -1,9 +0,0 @@
-import { AWSSecretsRetrieval } from "./AWSSecretsRetrieval.js";
-import { describe, expect, it } from "vitest";
-
-describe("AWSSecretsRetrieval", () => {
-  it("should retrieve secrets from AWS", async () => {
-    const secrets = await AWSSecretsRetrieval();
-    expect(secrets).toBeDefined();
-  });
-});
