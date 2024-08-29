8054b712f42208d5a22027f33e776e58c8f39d51
Andrew Monson <andrewmonsondev@gmail.com>
2024-08-29 09:42:25 -0400

    configured vitest to use env variables and added recoveryMail tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
 import { configDefaults, defineConfig } from "vitest/config";
+import { config } from "dotenv";
 
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, "packages/template/*"],
+    env: {
+      ...config({ path: ".env" }).parsed,
+    },
   },
 });
