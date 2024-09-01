4dff9dc32e51aefaa5bf116ccf8e00054e7e12da
Andrew Monson <andrewmonsondev@gmail.com>
2024-08-31 21:04:36 -0400

    additional schoolSchema tests

--- GraphQL/Schools/schoolSchema.test.ts
+++ GraphQL/Schools/schoolSchema.test.ts
@@ -1,41 +1,77 @@
 import { BaseContext } from "@apollo/server";
 import { ApolloServer } from "apollo-server-express";
+import { Request, Response } from "express";
 import { GraphQLFormattedError } from "graphql";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { schools as schoolData } from "../../data/schools.js";
-import { SchoolAttributes, SchoolModel } from "../../models/index.js";
+import {
+  ReviewModel,
+  SchoolAttributes,
+  SchoolModel,
+  UserAttributes,
+  UserModel,
+} from "../../models/index.js";
 import { schema } from "../schema.js";
 
 describe("School Resolvers", async () => {
   let con: typeof mongoose;
   let db: MongoMemoryServer;
   let schools: SchoolAttributes[];
+  let user: UserAttributes;
   let testServer: ApolloServer;
 
-  type SchoolResponse = {
-    school: SchoolAttributes;
+  interface SchoolResponse {
+    school: SchoolAttributes & {
+      latLng: {
+        lat: number;
+        lng: number;
+      };
+    };
     schools: {
       schools: SchoolAttributes[];
     };
     allSchools: SchoolAttributes[];
-  };
+    addSchool: SchoolAttributes;
+    deleteSchool: string;
+  }
 
-  type SingleGraphQLResponse<SchoolResponse> = {
+  interface SingleGraphQLResponse<SchoolResponse> {
     kind: string;
     data: SchoolResponse;
     errors: readonly GraphQLFormattedError[];
-  };
+  }
+
+  interface MyContext extends BaseContext {
+    user: UserAttributes;
+    req: Request;
+    res: Response;
+  }
 
   beforeAll(async () => {
     db = await MongoMemoryServer.create();
     con = await mongoose.connect(db.getUri(), {});
 
     schools = await SchoolModel.create(schoolData);
+    user = await UserModel.create({
+      username: "test",
+      email: "test@test.com",
+      password: "password",
+      isAdmin: true,
+    });
 
-    testServer = new ApolloServer<BaseContext>({
+    testServer = new ApolloServer<MyContext>({
       schema,
+      context: ({ req, res }) => {
+        return {
+          user: {
+            isAdmin: true,
+          },
+          req,
+          res,
+        };
+      },
     });
   });
 
@@ -65,8 +101,6 @@ describe("School Resolvers", async () => {
             }
           `,
         })) as SingleGraphQLResponse<SchoolResponse>;
-
-        console.log(response);
 
         expect(response.errors).toBe(undefined);
         expect(response.data?.school?.name).toBe(schools[0].name);
@@ -176,8 +210,21 @@ describe("School Resolvers", async () => {
   });
   describe("Mutations", async () => {
     describe("addSchool", async () => {
-      it("should create a new school", async () => {
-        const response = (await testServer.executeOperation({
+      it("should create a new school only if admin user requests", async () => {
+        const newServer = new ApolloServer<MyContext>({
+          schema,
+          context: ({ req, res }) => {
+            return {
+              user: {
+                isAdmin: true,
+              },
+              req,
+              res,
+            };
+          },
+        });
+
+        const response = (await newServer.executeOperation({
           query: `
             mutation {
               addSchool(
@@ -195,11 +242,195 @@ describe("School Resolvers", async () => {
               }
             }
           `,
-          // TODO: Add graphlQL context to login and run this test.
         })) as SingleGraphQLResponse<SchoolResponse>;
 
         expect(response.errors).toBe(undefined);
-        expect(response.data?.school?.name).toBe("Test School");
+        expect(response.data?.addSchool.name).toBe("Test School");
+      });
+
+      it("should return an error if a non-admin user requests", async () => {
+        const newServer = new ApolloServer<MyContext>({
+          schema,
+          context: ({ req, res }) => {
+            return {
+              user: {
+                isAdmin: false,
+              },
+              req,
+              res,
+            };
+          },
+        });
+
+        const response = (await newServer.executeOperation({
+          query: `
+            mutation {
+              addSchool(
+                name: "Test School"
+                address: "123 Test St."
+                city: "Test City"
+                state: "TS"
+                zipcode: "12345"
+              ) {
+                name
+                address
+                city
+                state
+                zipcode
+              }
+            }
+          `,
+        })) as SingleGraphQLResponse<SchoolResponse>;
+
+        expect(response.data).toBe(null);
+        expect(response.errors).not.toBe(undefined);
+      });
+
+      it("should return an error if the required fields are not provided", async () => {
+        const response = (await testServer.executeOperation({
+          query: `
+            mutation {
+              addSchool(
+                name: "Test School"
+                address: "123 Test St."
+                city: "Test City"
+                state: "TS"
+                zipcode: ""
+              ) {
+                name
+                address
+                city
+                state
+                zipcode
+              }
+            }
+          `,
+        })) as SingleGraphQLResponse<SchoolResponse>;
+
+        expect(response.data).toBe(null);
+        expect(response.errors).not.toBe(undefined);
+      });
+    });
+
+    describe("deleteSchool", async () => {
+      it("should delete a school by id only if admin user requests", async () => {
+        const newServer = new ApolloServer<MyContext>({
+          schema,
+          context: ({ req, res }) => {
+            return {
+              user: {
+                isAdmin: true,
+              },
+              req,
+              res,
+            };
+          },
+        });
+
+        const response = (await newServer.executeOperation({
+          query: `
+            mutation {
+              deleteSchool(id: "${schools[0].id}")
+            }
+          `,
+        })) as SingleGraphQLResponse<SchoolResponse>;
+
+        expect(response.errors).toBe(undefined);
+        expect(response.data?.deleteSchool).toBe("School deleted successfully");
+      });
+
+      it("should return an error if a non-admin user requests", async () => {
+        const newServer = new ApolloServer<MyContext>({
+          schema,
+          context: ({ req, res }) => {
+            return {
+              user: {
+                isAdmin: false,
+              },
+              req,
+              res,
+            };
+          },
+        });
+
+        const response = (await newServer.executeOperation({
+          query: `
+            mutation {
+              deleteSchool(id: "${schools[0].id}")
+            }
+          `,
+        })) as SingleGraphQLResponse<SchoolResponse>;
+
+        expect(response.data.deleteSchool).toBe(null);
+        expect(response.errors).not.toBe(undefined);
+      });
+
+      it("should return an error if the school id is not provided", async () => {
+        const response = (await testServer.executeOperation({
+          query: `
+            mutation {
+              deleteSchool(id: "")
+            }
+          `,
+        })) as SingleGraphQLResponse<SchoolResponse>;
+
+        expect(response.data.deleteSchool).toBe(null);
+        expect(response.errors).not.toBe(undefined);
+      });
+    });
+  });
+  describe("School subresolvers", async () => {
+    describe("getLatLng", async () => {
+      it("should return a latLng object with lat and lng properties", async () => {
+        const response = (await testServer.executeOperation({
+          query: `
+            query {
+              school(id: "${schools[1].id}") {
+                latLng {
+                  lat
+                  lng
+                }
+              }
+            }
+          `,
+        })) as SingleGraphQLResponse<SchoolResponse>;
+
+        expect(response.errors).toBe(undefined);
+        expect(response.data?.school?.latLng).toHaveProperty("lat");
+        expect(response.data?.school?.latLng).toHaveProperty("lng");
+      });
+    });
+
+    describe("reviews", async () => {
+      it("should return reviews for a school", async () => {
+        const newReview = await ReviewModel.create({
+          rating: 5,
+          review: "Great school",
+          owner: user.id,
+          school: schools[1].id,
+        });
+
+        await SchoolModel.findByIdAndUpdate(
+          { _id: schools[1].id },
+          { $addToSet: { reviews: newReview.id } },
+          { new: true },
+        );
+
+        const response = (await testServer.executeOperation({
+          query: `
+            query {
+              school(id: "${schools[1].id}") {
+                reviews {
+                  rating
+                  review
+                }
+              }
+            }
+          `,
+        })) as SingleGraphQLResponse<SchoolResponse>;
+
+        expect(response.errors).toBe(undefined);
+        expect(response.data?.school?.reviews.length).toBe(1);
       });
     });
   });
